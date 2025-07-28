d468ebe7c252b4ca053904a4812239e994d6e9f7
Kazi Tanvirul Islam Sakib <ktisakib@gmail.com>
2025-07-29 01:29:47 +0600

    feat: home page hint

--- apps/web/app/[locale]/(home)/page.tsx
+++ apps/web/app/[locale]/(home)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@enterprise/ui/components/button"
 
+import Link from "next/link"
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold max-w-2xs">Click the link to go to the course page use the search bar to go to any course page  </h1>
+        <Link href="/product/ielts-course" >IELTS Course</Link>
       </div>
     </div>
   )
